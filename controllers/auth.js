a4ac635fc0a4ce55ed99597825e9b689b7df7bc5
Kashmiri-123 <kmonimahanta@gmail.com>
2020-03-19 14:36:45 +0530

    auth corrected

--- controllers/auth.js
+++ controllers/auth.js
@@ -101,7 +101,7 @@ exports.isSignedIn =  expressJwt({
 
 //customed middlewares
 exports.isAuthenticated = (req,res, next) => {
-    let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+    let checker = req.profile && req.auth && req.profile._id == req.auth._id;
 
     if (!checker)
     {
